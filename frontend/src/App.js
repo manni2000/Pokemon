9db3bc4578f94ab855a9221db753244fd755f050
Manish kumar <manishmandal9734@gmail.com>
2024-08-05 19:15:12 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import AddPokemonPage from './components/AddPokemonPage';
 import ListPokemonPage from './components/ListPokemonPage';
 import HomePage from './components/HomePage';
 import PokemonDetails from './components/PokemonDetails';
-import './index.css';  // Ensure global styles are imported
+import './index.css'; 
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <App />
-    </Router>,
+    </Router>
   </React.StrictMode>
 );
 
