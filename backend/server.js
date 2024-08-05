3f64e59bd49a94fe5c78bbbf2c69e418d28493d6
Manish kumar <manishmandal9734@gmail.com>
2024-08-05 19:40:47 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -7,24 +7,17 @@ const pokemonRoutes = require('./routes/pokemonRoutes');
 const app = express();
 const PORT = 5000;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Serve static files from the React app (if you have a build folder)
-// This is useful if you want to serve your frontend from the same server
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// API Routes
 app.use('/api/pokemon', pokemonRoutes);
 
-// The "catchall" handler: for any request that doesn't match the above, send back the React app's index.html file.
-// This is useful for client-side routing
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
