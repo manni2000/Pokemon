724abc366b163debeebcea678fcde7750871b308
Manish kumar <manishmandal9734@gmail.com>
2024-08-06 11:31:51 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const pokemonRoutes = require('./routes/pokemonRoutes');
-
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.use('/api/pokemon', pokemonRoutes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
